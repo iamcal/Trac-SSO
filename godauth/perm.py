2c60c17cdc4228a7b25509a9ebd3fa62d57190ec
root <root@domU-12-31-39-14-F4-6C.compute-1.internal>
2010-09-08 19:35:39 -0400

    this still doesn't work - will need to be re-thought

--- godauth/perm.py
+++ godauth/perm.py
@@ -2,7 +2,7 @@ from trac.core import *
 from trac.perm import IPermissionGroupProvider 
 from trac.util import TracError
 
-class FlAuthPermissionGroupProvider(Component):
+class GodAuthPermissionGroupProvider(Component):
 
     implements(IPermissionGroupProvider)
 
@@ -11,8 +11,18 @@ class FlAuthPermissionGroupProvider(Component):
         if username == 'Anonymous':
             return groups
 
-        if username == 'cal':
-            groups.append('admins')
+        # gah! we don't actually get passed the reqeust, since trac works in an
+        # odd way. this is going to require some re-engineering
+
+        #user = req.get_header("godauth-user")
+        #roles = req.get_header("godauth-roles")
+        user = None
+        roles = None
+
+        if user is not None:
+            if roles is not None:
+                roles = roles.split(',')
+                for role in roles:
+                    groups.append(role)
 
         return groups
-
